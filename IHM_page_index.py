d2df9e316bad4213d2d4d172407fab96eb251b68
RomainMarcel <romainmarcel26@gmail.com>
2019-03-15 16:08:27 +0100

    maj romain

--- IHM_page_index.py
+++ IHM_page_index.py
@@ -1,5 +1,6 @@
 from bottle import run, route, template
 import models as mo
+import peewee
 
 
 
@@ -32,11 +33,24 @@ def statperday():
     return template('Statperday', resultliste=mo.StatsPerDay.select())
 
 
-@route('/Statpermatch')
+@route('/Statpermatch')  # page stat globale
 def statpermatch():
 
     return template('Statpermatch.tpl', results_list=mo.StatsPerMatch.select())
 
 
+@route('/Statpermatch/machine/<machine_name>')  # page stat par machine
+def statpermatch(machine_name):
+
+    return template('Statpermatch.tpl', results_list=mo.StatsPerMatch.select().where(mo.StatsPerMatch.machine_name == mo.GameServer.get(mo.GameServer.nom_serveur == machine_name)))
+
+
+@route('/Statpermatch/date/<date>')  # page stat par date
+def statpermatch(date):
+
+    return template('Statpermatch_date.tpl', results_list=mo.StatsPerMatch.select().where(mo.StatsPerMatch.start_game == date ).order_by(mo.StatsPerMatch.start_game))
+
+
 if __name__ == '__main__':
     run(debug=True, reloader=True)
+
