db1193bbd4521ec073a59273ca891088ed971276
ztribeche <t_zouhaira@hotmail.com>
2019-03-15 10:05:51 +0100

    version IHM avec zou

--- IHM_page_index.py
+++ IHM_page_index.py
@@ -1,21 +1,33 @@
-from bottle import run, route , template
+from models import *
+from bottle import run, route, template
 
 
 @route('/')
 def index():
     return template('index')
 
+
 @route('/gameserver')
 def gameserver():
     return '<h1>Game Server Page </h1>'
 
+
 @route('/lastgameresult')
 def lastgameresult():
-    return '<h1> Last Game Result Page </h1>'
+    last_game = {
+        "machine_name": "A",
+        "start_game": "12/02/2019",
+        "duree": "120",
+        "winner": "Player 1",
+    }
+    last_game = StatsPerMatch.select().order_by(StatsPerMatch.start_game.desc())[0]
+    return template('lastgameresultpage', game=last_game)
+
 
 @route('/Statperday')
 def statperday():
     return '<h1> Stat per day page <h1> '
+
 
 @route('/Statpermatch')
 def statpermatch():
